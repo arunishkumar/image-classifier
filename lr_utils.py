eef88bf373655d2b22108a5d0c819a28c57ac4c8
Arunish kumar <43927484+arunishkumar@users.noreply.github.com>
2019-10-12 17:54:18 +0530

    Update lr_utils.py

--- lr_utils.py
+++ lr_utils.py
@@ -7,6 +7,8 @@ def load_dataset():
     train_set_x_orig = np.array(train_dataset["train_set_x"][:]) # your train set features
     train_set_y_orig = np.array(train_dataset["train_set_y"][:]) # your train set labels
     
+    test_dataset = h5py.File('datasets/test_catvnoncat.h5', "r")
+    classes = np.array(test_dataset["list_classes"][:]) # the list of classes
     train_set_y_orig = train_set_y_orig.reshape((1, train_set_y_orig.shape[0]))
     
     return train_set_x_orig, train_set_y_orig
