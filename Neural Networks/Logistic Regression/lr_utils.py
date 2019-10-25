ca553be51e6b1ed9201125ab98b2ef30d9920dcb
Arunish kumar <43927484+arunishkumar@users.noreply.github.com>
2019-10-26 00:18:45 +0530

    Add files via upload

---
+++ Neural Networks/Logistic Regression/lr_utils.py
@@ -0,0 +1,15 @@
+import numpy as np
+import h5py
+    
+    
+def load_dataset():
+    train_dataset = h5py.File('datasets/train_catvnoncat.h5', "r")
+    train_set_x_orig = np.array(train_dataset["train_set_x"][:]) # your train set features
+    train_set_y_orig = np.array(train_dataset["train_set_y"][:]) # your train set labels
+
+    classes = np.array(train_dataset["list_classes"][:]) # the list of classes
+    
+    train_set_y_orig = train_set_y_orig.reshape((1, train_set_y_orig.shape[0]))
+    
+    return train_set_x_orig, train_set_y_orig, classes
+
